0c33d536ff94a6e56437993da636fc5492d325c2
CaioWF <caioweliton@alu.ufc.br>
2021-12-25 12:24:50 -0300

    chore: add typeorm & your configuration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'reflect-metadata';
 
+import createConnection from '@shared/infra/typeorm';
+
+createConnection();
 const app = express();
 
 app.use(express.json());
