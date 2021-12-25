a25c4019b3dbfceca855c71b48722fb683b7b702
CaioWF <caioweliton@alu.ufc.br>
2021-12-25 19:17:26 -0300

    refactor: verify if city already exists on create

--- src/modules/cities/useCases/CreateCityUseCase.ts
+++ src/modules/cities/useCases/CreateCityUseCase.ts
@@ -1,3 +1,5 @@
+import { AppError } from '@shared/errors/AppError';
+
 import { ICreateCityDTO } from '../dtos/ICreateCityDTO';
 import { City } from '../infra/typeorm/entities/City';
 import { ICitiesRepository } from '../repositories/ICitiesRepository';
@@ -6,6 +8,13 @@ class CreateCityUseCase {
   constructor(private citiesRepository: ICitiesRepository) {}
 
   async execute({ name, state }: ICreateCityDTO): Promise<City> {
+    const cityAlreadyxists = await this.citiesRepository.exists({
+      name,
+      state,
+    });
+
+    if (cityAlreadyxists) throw new AppError('City already exists', 409);
+
     const city = await this.citiesRepository.create({ name, state });
     return city;
   }
