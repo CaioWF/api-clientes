8592c760cfcb4c03b1d91eb8fd11f04c48560d6c
CaioWF <caioweliton@alu.ufc.br>
2021-12-25 17:35:41 -0300

    refactor: add test database image & remove logics from typeorm/index

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,6 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (host = 'database_clientes'): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
-  return createConnection(
-    Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
-      database:
-        process.env.NODE_ENV === 'test'
-          ? 'clientes_test'
-          : defaultOptions.database,
-    }),
-  );
+  const defaultOptions = await getConnectionOptions('default');
+  return createConnection(Object.assign(defaultOptions, { host }));
 };
