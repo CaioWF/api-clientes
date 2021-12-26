bcc6686b13f04c017b28d9832730a59e56037378
CaioWF <caioweliton@alu.ufc.br>
2021-12-25 21:47:42 -0300

    feat: add client entity

---
+++ src/modules/clients/infra/typeorm/entities/Client.ts
@@ -0,0 +1,39 @@
+import { randomUUID } from 'crypto';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+import { City } from '@modules/cities/infra/typeorm/entities/City';
+
+@Entity('clients')
+class Client {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  full_name: string;
+
+  @Column()
+  sex: string;
+
+  @Column()
+  birth_date: Date;
+
+  @ManyToOne(() => City)
+  @JoinColumn({ name: 'city_id' })
+  city: City;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
+
+export { Client };
