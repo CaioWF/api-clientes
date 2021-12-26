83082b71911706a88909de5ffc900f594286531b
CaioWF <caioweliton@alu.ufc.br>
2021-12-26 11:04:00 -0300

    test: ensure that it is possible to create a client

---
+++ src/modules/clients/useCases/CreateClientUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { IClientsRepository } from '../repositories/IClientsRepository';
+import { ClientsRepositoryInMemory } from '../repositories/inMemory/ClientsRepositoryInMemory';
+import { CreateClientUseCase } from './CreateClientUseCase';
+
+describe('CreateClientUseCase', () => {
+  let clientsRepositoryInMemory: IClientsRepository;
+  let createClientUseCase: CreateClientUseCase;
+
+  beforeEach(() => {
+    clientsRepositoryInMemory = new ClientsRepositoryInMemory();
+    createClientUseCase = new CreateClientUseCase(clientsRepositoryInMemory);
+  });
+
+  it('should be able to create a new client', async () => {
+    const client = await createClientUseCase.execute({
+      full_name: 'any_name',
+      sex: 'M',
+      birth_date: new Date(),
+      city_id: 'any_city_id',
+    });
+
+    expect(client).toHaveProperty('id');
+    expect(client).toHaveProperty('created_at');
+  });
+});
