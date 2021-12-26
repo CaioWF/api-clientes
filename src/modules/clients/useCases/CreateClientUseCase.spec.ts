7894597bfc58c0e53aa46ae54994b640c88d3a03
CaioWF <caioweliton@alu.ufc.br>
2021-12-26 12:27:16 -0300

    test: ensure that it is not possible to create client with unmapped gender

--- src/modules/clients/useCases/CreateClientUseCase.spec.ts
+++ src/modules/clients/useCases/CreateClientUseCase.spec.ts
@@ -1,3 +1,6 @@
+import { AppError } from '@shared/errors/AppError';
+
+import { GenderType } from '../infra/typeorm/entities/Client';
 import { IClientsRepository } from '../repositories/IClientsRepository';
 import { ClientsRepositoryInMemory } from '../repositories/inMemory/ClientsRepositoryInMemory';
 import { CreateClientUseCase } from './CreateClientUseCase';
@@ -14,12 +17,23 @@ describe('CreateClientUseCase', () => {
   it('should be able to create a new client', async () => {
     const client = await createClientUseCase.execute({
       full_name: 'any_name',
-      gender: 'M',
+      gender: GenderType.MALE,
       birth_date: new Date(),
       city_id: 'any_city_id',
     });
 
     expect(client).toHaveProperty('id');
     expect(client).toHaveProperty('created_at');
+  });
+
+  it('should not be able to create a new client with unmapped gender', async () => {
+    await expect(
+      createClientUseCase.execute({
+        full_name: 'any_name',
+        gender: 'unmapped_gender',
+        birth_date: new Date(),
+        city_id: 'any_city_id',
+      }),
+    ).rejects.toEqual(new AppError('Unprocessable entity error', 422));
   });
 });
