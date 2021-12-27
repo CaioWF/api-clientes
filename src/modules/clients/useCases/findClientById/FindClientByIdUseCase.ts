86eb48e58ea4cc18588c88b0a63f1ab1e9de52a9
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 17:30:55 -0300

    feat: add uuid validator

--- src/modules/clients/useCases/findClientById/FindClientByIdUseCase.ts
+++ src/modules/clients/useCases/findClientById/FindClientByIdUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import validator from 'validator';
 
 import { Client } from '@modules/clients/infra/typeorm/entities/Client';
 import { IClientsRepository } from '@modules/clients/repositories/IClientsRepository';
@@ -12,6 +13,8 @@ class FindClientByIdUseCase {
   ) {}
 
   async execute(id: string): Promise<Client> {
+    if (!validator.isUUID(id)) throw new AppError('Invalid id', 400);
+
     const client = await this.clientsRepository.findById(id);
 
     if (!client) throw new AppError('User not found', 404);
