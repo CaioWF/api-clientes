221d4ca791767de47bd319107d21cbccf63a4491
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 10:24:32 -0300

    feat: add post client route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { citiesRoutes } from './city.routes';
+import { clientsRoutes } from './client.routes';
 
 const router = Router();
 
 router.use('/cities', citiesRoutes);
+router.use('/clients', clientsRoutes);
 
 export { router };
