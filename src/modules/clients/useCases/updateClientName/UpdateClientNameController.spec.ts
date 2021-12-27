be4fae3a620acf60bf3396b5e06fb5d93b33b9a0
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 20:19:10 -0300

    test: ensure that it is possible to update a client name by route request

---
+++ src/modules/clients/useCases/updateClientName/UpdateClientNameController.spec.ts
@@ -0,0 +1,50 @@
+import { randomUUID } from 'crypto';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+
+import { City } from '@modules/cities/infra/typeorm/entities/City';
+import { GenderType } from '@modules/clients/infra/typeorm/entities/Client';
+import createConnection from '@shared/infra/typeorm';
+
+import { app } from '../../../../app';
+
+let connection: Connection;
+let sharedCity: City;
+describe('UpdateClientNameController', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const response = await request(app)
+      .post('/cities')
+      .send({ name: 'Shared city', state: 'Shared state' });
+    sharedCity = response.body;
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to update a client name', async () => {
+    const {
+      body: { id },
+    } = await request(app)
+      .post('/clients')
+      .send({
+        full_name: 'any_name',
+        gender: GenderType.MALE,
+        birth_date: '2000-01-01',
+        city_id: sharedCity.id,
+      })
+      .expect(201);
+
+    const response = await request(app).patch(`/clients/${id}`).send({
+      full_name: 'another_name',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toEqual(id);
+    expect(response.body.full_name).toEqual('another_name');
+  });
+});
