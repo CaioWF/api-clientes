8ab08f76291018192237882808da6f25694a832a
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 12:20:44 -0300

    test: ensure that it is possible to list cities paginated filtered by name

--- src/modules/cities/useCases/listCities/ListCityUseCase.spec.ts
+++ src/modules/cities/useCases/listCities/ListCityUseCase.spec.ts
@@ -25,4 +25,24 @@ describe('ListCityUseCase', () => {
     expect(listPaginated.cities).toEqual([city]);
     expect(listPaginated.pagination).toEqual({ skip: 0, take: 10 });
   });
+
+  it('should be able to list cities paginated filtering by name', async () => {
+    const city = await citiesRepositoryInMemory.create({
+      name: 'city that return',
+      state: 'state list',
+    });
+    await citiesRepositoryInMemory.create({
+      name: 'city that not return',
+      state: 'state list',
+    });
+
+    const listPaginated = await listCityUseCase.execute({
+      filters: { name: 'that return' },
+    });
+
+    expect(listPaginated).toHaveProperty('cities');
+    expect(listPaginated).toHaveProperty('pagination');
+    expect(listPaginated.cities).toEqual([city]);
+    expect(listPaginated.pagination).toEqual({ skip: 0, take: 10 });
+  });
 });
