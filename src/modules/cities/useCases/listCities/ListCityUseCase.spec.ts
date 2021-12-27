707673a2d3a7ee67bc44585c4432cb7091da4a3c
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 12:15:32 -0300

    test: ensure that it is possible to list cities paginated

---
+++ src/modules/cities/useCases/listCities/ListCityUseCase.spec.ts
@@ -0,0 +1,28 @@
+import { ICitiesRepository } from '@modules/cities/repositories/ICitiesRepository';
+import { CitiesRepositoryInMemory } from '@modules/cities/repositories/inMemory/CitiesRepositoryInMemory';
+
+import { ListCityUseCase } from './ListCityUseCase';
+
+describe('ListCityUseCase', () => {
+  let citiesRepositoryInMemory: ICitiesRepository;
+  let listCityUseCase: ListCityUseCase;
+
+  beforeEach(() => {
+    citiesRepositoryInMemory = new CitiesRepositoryInMemory();
+    listCityUseCase = new ListCityUseCase(citiesRepositoryInMemory);
+  });
+
+  it('should be able to list cities paginated', async () => {
+    const city = await citiesRepositoryInMemory.create({
+      name: 'city list',
+      state: 'state list',
+    });
+
+    const listPaginated = await listCityUseCase.execute({ filters: {} });
+
+    expect(listPaginated).toHaveProperty('cities');
+    expect(listPaginated).toHaveProperty('pagination');
+    expect(listPaginated.cities).toEqual([city]);
+    expect(listPaginated.pagination).toEqual({ skip: 0, take: 10 });
+  });
+});
