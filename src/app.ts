e774b955f3571c4b8308f6ff3a45b53e221887c4
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 10:23:59 -0300

    fix: async error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
 
@@ -16,14 +16,16 @@ app.use(express.json());
 
 app.use(router);
 
-app.use((err: Error, request: Request, response: Response) => {
-  if (err instanceof AppError)
-    return response.status(err.statusCode).json({ message: err.message });
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError)
+      return response.status(err.statusCode).json({ message: err.message });
 
-  return response.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  });
-});
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
 
 export { app };
