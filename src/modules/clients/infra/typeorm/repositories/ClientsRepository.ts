f8ff9a84d82589a53fd3701d7e060c4959596690
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 18:14:33 -0300

    feat: add delete on client repository typeorm

--- src/modules/clients/infra/typeorm/repositories/ClientsRepository.ts
+++ src/modules/clients/infra/typeorm/repositories/ClientsRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository } from 'typeorm';
+import { DeleteResult, getRepository, Repository } from 'typeorm';
 
 import { ICreateClientDTO } from '@modules/clients/dtos/ICreateClientDTO';
 import { IClientsRepository } from '@modules/clients/repositories/IClientsRepository';
@@ -22,6 +22,10 @@ class ClientsRepository implements IClientsRepository {
     const client = await this.repository.findOne(id);
 
     return client;
+  }
+
+  async delete(id: string): Promise<void | DeleteResult> {
+    return this.repository.delete(id);
   }
 }
 
