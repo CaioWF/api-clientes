842e597381ee63e65a604b53630771399734b593
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 16:13:28 -0300

    feat: add find client by id controller & route

--- src/shared/infra/http/routes/client.routes.ts
+++ src/shared/infra/http/routes/client.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { FindClientByIdController } from '@modules/clients/useCases/findClientById/FindClientByIdController';
 
 const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
+const findClientByIdController = new FindClientByIdController();
 
 clientsRoutes.post('/', createClientController.handle);
+clientsRoutes.get('/:id', findClientByIdController.handle);
 
 export { clientsRoutes };
