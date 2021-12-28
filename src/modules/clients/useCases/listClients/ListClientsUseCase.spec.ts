8b9367d5b632dd9ad31959f52760d68926b48d10
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 21:00:57 -0300

    test: ensure that it is possible to list clients paginated taking clients

--- src/modules/clients/useCases/listClients/ListClientsUseCase.spec.ts
+++ src/modules/clients/useCases/listClients/ListClientsUseCase.spec.ts
@@ -74,4 +74,26 @@ describe('UpdateClientNameUseCase', () => {
     expect(listPaginated.clients).toEqual([client]);
     expect(listPaginated.pagination).toEqual({ skip: 1, take: 10 });
   });
+
+  it('should be able to list clients paginated taking some clients', async () => {
+    const client = await clientsRepositoryInMemory.create({
+      full_name: 'any_name',
+      gender: GenderType.MALE,
+      birth_date: new Date(),
+      city_id: 'any_city_id',
+    });
+    await clientsRepositoryInMemory.create({
+      full_name: 'another_name',
+      gender: GenderType.MALE,
+      birth_date: new Date(),
+      city_id: 'any_city_id',
+    });
+
+    const listPaginated = await listClientsUseCase.execute({ take: 1 });
+
+    expect(listPaginated).toHaveProperty('clients');
+    expect(listPaginated).toHaveProperty('pagination');
+    expect(listPaginated.clients).toEqual([client]);
+    expect(listPaginated.pagination).toEqual({ skip: 0, take: 1 });
+  });
 });
