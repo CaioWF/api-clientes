4dfe97958ee29ad2aafb4f67b205b57a94f7b27d
CaioWF <caioweliton@alu.ufc.br>
2021-12-27 21:24:17 -0300

    test: ensure that it is possible to list clients paginated filtering by full name by route request

--- src/modules/clients/useCases/listClients/ListClientsController.spec.ts
+++ src/modules/clients/useCases/listClients/ListClientsController.spec.ts
@@ -48,4 +48,36 @@ describe('ListClientsController', () => {
     expect(response.body.clients[0]).toHaveProperty('id');
     expect(response.body.clients[0].full_name).toEqual('any_name');
   });
+
+  it('should be able to list clients paginated filtering by full name', async () => {
+    await request(app)
+      .post('/clients')
+      .send({
+        full_name: 'any_name',
+        gender: GenderType.MALE,
+        birth_date: '2000-01-01',
+        city_id: sharedCity.id,
+      })
+      .expect(201);
+
+    await request(app)
+      .post('/clients')
+      .send({
+        full_name: 'another_name',
+        gender: GenderType.MALE,
+        birth_date: '2000-01-01',
+        city_id: sharedCity.id,
+      })
+      .expect(201);
+
+    const response = await request(app)
+      .get('/clients')
+      .query({ full_name: 'any' });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('clients');
+    expect(response.body).toHaveProperty('pagination');
+    expect(response.body.clients[0]).toHaveProperty('id');
+    expect(response.body.clients[0].full_name).toEqual('any_name');
+  });
 });
